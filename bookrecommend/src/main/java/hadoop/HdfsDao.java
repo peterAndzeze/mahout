94c16fa3bdc45111c3184b84421547861b9abe96
peterAndzeze <494296312@qq.com>
2018-03-14 17:58:15 +0800

    配置hadoop

--- bookrecommend/src/main/java/hadoop/HdfsDao.java
+++ bookrecommend/src/main/java/hadoop/HdfsDao.java
@@ -1,6 +1,14 @@
 package hadoop;
 
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobConf;
+
+import java.io.IOException;
+import java.net.URI;
 
 public class HdfsDao {
     private static final String HDFS="hdfs://localhost:9000";
@@ -13,15 +21,40 @@ public class HdfsDao {
         this.conf = conf;
     }
 
+    public HdfsDao() {
+    }
+
     private String hdfsPath;
     private Configuration conf;
 
-    public void JobConf config(){
-        JobConf conf = new JobConf(HdfsDAO.class);
-        conf.setJobName("HdfsDAO");
-        conf.addResource("classpath:/hadoop/core-site.xml");
-        conf.addResource("classpath:/hadoop/hdfs-site.xml");
-        conf.addResource("classpath:/hadoop/mapred-site.xml");
-        return conf;
+
+    public static JobConf config(){
+       JobConf jobConf = new JobConf(HdfsDao.class);
+        jobConf.setJobName("HdfsDAO");
+        jobConf.addResource("classpath:/hadoop/core-site.xml");
+        jobConf.addResource("classpath:/hadoop/hdfs-site.xml");
+        jobConf.addResource("classpath:/hadoop/mapred-site.xml");
+        return jobConf;
     }
+
+    public static void main(String [] args){
+        JobConf jobConf= HdfsDao.config();
+        HdfsDao hdfsDao=new HdfsDao(jobConf);
+        Path path = new Path("/user/newcapec/");
+        FileSystem fs = null;
+        try {
+            fs = FileSystem.get(URI.create(hdfsDao.hdfsPath), jobConf);
+
+        FileStatus[] list = fs.listStatus(path);
+
+        for (FileStatus f : list) {
+            System.out.printf("name: %s, folder: %s, size: %d\n", f.getPath(), f.isDir(), f.getLen());
+        }
+        System.out.println("==========================================================");
+        fs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
